3b8e01856aeedaf0d4db3c9eeaa7a665e8963dc8
Ajay Verma <ajay7monu@gmail.com>
2022-08-13 13:27:22 +0530

    file added

---
+++ 12_Day_Modules/main.py
@@ -0,0 +1,9 @@
+
+from day_12 import generate_full_name as fullname, sum_two_nums as total, person as p, gravity as g
+print(fullname('Ajay','Verma'))
+print(total(1, 9))
+mass = 100
+weight = mass * g
+print(weight)
+print(p)
+print(p['firstname'])
